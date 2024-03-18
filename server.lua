51be7c09576cf8cd7a2a1f94c25c5f2d81ff368c
JoeV2 <146123038+Joe-Development@users.noreply.github.com>
2024-03-18 09:09:52 -0500

    Update server.lua

--- server.lua
+++ server.lua
@@ -20,37 +20,3 @@ end)
 
 
 
-
-
-
-
-
-
-
-
-
-
-
---
-CreateThread(function()
-    
-    if Config.CheckForUpdates then
-        -- https://github.com/Blumlaut/FiveM-Resource-Version-Check-Thing/
-        updatePath = "/Joe-Development/Donator-Menu" -- your git user/repo path
-        resourceName = "^6chroma-enginesoundmenu" -- the resource name
-        local function checkVersion(err,responseText, headers)
-            curVersion = LoadResourceFile(GetCurrentResourceName(), "version/version.txt") -- make sure the "version" file actually exists in your resource root!
-            if curVersion ~= responseText and tonumber(curVersion) < tonumber(responseText) then
-                print("\n"..resourceName.." ^1is outdated, please update it from:\n^3https://github.com/Joe-Development/Donator-Menu/releases/latest\n^1For support or issues, please visit ^3https://discord.gg/sNHg4X7xt2^7")
-            else
-                print("\n"..resourceName.." ^2is up to date, and has been loaded - enjoy!\nFor support or issues, please visit ^3https://discord.gg/sNHg4X7xt2^7")
-            end
-        end
-        
-        PerformHttpRequest("https://raw.githubusercontent.com"..updatePath.."/main/Donator-Menu/version/version.txt", checkVersion, "GET")
-
-    else
-        print("\n^6Doantor-Menu ^2has been loaded - enjoy! ^1[VERSION CHECK DISABLED]\n^2For support or issues, please visit ^3https://discord.gg/sNHg4X7xt2^7")
-    end
-
-end)
